745522875735f9058e5be07b92152d713f5e1a8d
Eronildes da Silva Reis <entusiastati@outlook.com>
2025-01-26 00:02:22 +0000

    Jogo_de_Cartas_Super_Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-
+// Definição da struct Carta
 typedef struct {
-    char codigo[4];     
-    int populacao;     
-    float area;        
-    float pib;       
-    int pontosTuristicos; 
+    char codigo[4];     // Código da carta (ex: A01)
+    int populacao;      // População da cidade
+    float area;         // Área da cidade
+    float pib;          // PIB da cidade
+    int pontosTuristicos; // Número de pontos turísticos
 } Carta;
 
 #define NUM_CARTAS 32
@@ -16,22 +16,23 @@ void cadastrarCartas(Carta cartas[], int tamanho) {
     for (int i = 0; i < tamanho; i++) {
         printf("\n---- Cadastro da Carta %d ----\n", i + 1);
         
-        
-        printf("Digite o código da cidade: ");
+        // Código da carta
+        printf("Digite o código da cidade (ex: A01): ");
         scanf("%s", cartas[i].codigo);
 
-
+        // População
         printf("Digite a população: ");
         scanf("%d", &cartas[i].populacao);
 
-
+        // Área
         printf("Digite a área (em km²): ");
         scanf("%f", &cartas[i].area);
 
-
+        // PIB
         printf("Digite o PIB (em milhões): ");
         scanf("%f", &cartas[i].pib);
 
+        // Número de pontos turísticos
         printf("Digite o número de pontos turísticos: ");
         scanf("%d", &cartas[i].pontosTuristicos);
     }
@@ -54,11 +55,12 @@ int main() {
 
     printf("Bem-vindo ao cadastro do Super Trunfo - Países!\n");
 
-
+    // Cadastrar as cartas
     cadastrarCartas(cartas, NUM_CARTAS);
 
-
+    // Exibir as cartas cadastradas
     exibirCartas(cartas, NUM_CARTAS);
 
     return 0;
+}
 }
